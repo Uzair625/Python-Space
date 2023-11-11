29b7670814a370b0ae12c85e621da73bac601feb
Uzair Ali <123838562+Uzair625@users.noreply.github.com>
2023-11-11 07:21:21 -0800

    Add files via upload
    Signed-off-by: Uzair Ali <123838562+Uzair625@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,64 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except sr.UnknownValueError:
+        print("Sorry, I didn't catch that.")
+        command = ""
+    except sr.RequestError:
+        print('Sorry, my speech service is down.')
+        command = ""
+    return command
+
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        current_time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('The current time is ' + current_time)
+    elif 'who the heck is' in command:
+        person = command.replace('who the heck is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'date' in command:
+        talk('Sorry, I have a headache.')
+    elif 'are you single' in command:
+        talk('I am in a relationship with Wi-Fi.')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    else:
+        talk('Please say the command again.')
+
+
+while True:
+    run_alexa()
